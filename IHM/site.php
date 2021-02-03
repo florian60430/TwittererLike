10799abb92ea9e7bd067ce557d9d13a3aca7eb16
florian60430 <florian.wantelez@hotmail.com>
2021-02-03 11:44:34 +0100

    formulaire envoyer

--- IHM/site.php
+++ IHM/site.php
@@ -3,15 +3,26 @@
 
 <div class="mainStream" id="stream"></div>
 
-<input type="text" name="tweet" size="50px" placeholder="What's happening ?"> 
+<input id ="tweet" type="text" name="InputTweet" size="50px" placeholder="What's happening ?"> 
 <button name="btnSubmit" id="submit">Envoyer</button>
 
 
 
 <script> 
 
+let button = document.getElementById("submit");
+let tweetContent = document.getElementById("stream");
+let input = document.getElementById("tweet");
 
 
+
+
+input.addEventListener("change", function(event) {
+    
+    button.addEventListener("click", function(){
+        tweetContent.innerHTML = "<div>"+event.target.value+"</div>";
+    });
+});
 </script>
 
 
