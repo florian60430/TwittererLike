ba5969dd3331704f676154b83e1c9d1a641c6c09
florian60430 <florian.wantelez@hotmail.com>
2021-02-03 11:20:52 +0100

    site.php stream

--- IHM/site.php
+++ IHM/site.php
@@ -1,13 +1,18 @@
-<?php require ("entete.html"); ?>
+<?php include 'entete.html' ?>
+
+
+<div class="mainStream" id="stream"></div>
+
+<input type="text" name="tweet" size="50px" placeholder="What's happening ?"> 
+<button name="btnSubmit" id="submit">Envoyer</button>
 
 
 
+<script> 
 
 
 
-
-
-
+</script>
 
 
 
