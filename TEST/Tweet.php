3f19ada2b2b64d289e6ee554c507ed23eab3e022
FGarcia98 <fgarcia@la-providence.net>
2021-02-05 16:45:53 +0100

    test tweet

--- TEST/Tweet.php
+++ TEST/Tweet.php
@@ -1,26 +1,32 @@
 <?php
+include '../BDD/classUser.php';
 include '../BDD/classtweet.php';
 include '../BDD/config.php';
 
 
 
-?> <h2> Tweet </h2>
+?> <h2> GET Tweet </h2>
 <?php 
 $tweet = new tweet($bdd);
-$tweet->init()."<br>";
+$tweet->init(1)."<br>";
 
-echo $tweet->getIdtweet(1)."<br>";
-echo $tweet->getContenu()."<br>";
-echo $tweet->getDate()."<br>";
-echo $tweet->getUser(1)."<br>";
-echo $tweet->getNumberLikes()."<br>";
+echo "ID TWEET:  <br>".$tweet->getIdtweet()."<br>";
+echo "CONTENU TWEET: <br> ".$tweet->getContenu()."<br>";
+echo "DATE TWEET:  <br>".$tweet->getDate()."<br>";
+echo "PSEUDO USER:  <br>".$tweet->getUser()->getPseudo()."<br>";
+echo "IDENTIFIANT USER: <br> ".$tweet->getUser()->getIdentifiant()."<br>";
+echo "NUMBER LIKE:  <br>".$tweet->getNumberLikes()."<br>";
+?>
+<button type="button" class="btn btn-danger">LIKE</button>
+<?php
+echo"</br>";
+echo" <h2> SET Tweet </h2>";
 
 
-
-echo $tweet->setIdtweet(36)."<br>";
-echo $tweet->setContenu("Goudal la mandal")."<br>";
-echo $tweet->setDate("2021-03-05")."<br>";
-echo $tweet->setUser(6)."<br>";
+echo "ID TWEET:  <br>".$tweet->setIdtweet(36)."<br>";
+echo "CONTENU TWEET:  <br>".$tweet->setContenu("Goudal la mandal")."<br>";
+echo "DATE TWEET:  <br>".$tweet->setDate("2021-03-05")."<br>";
+echo "USER TWEET:  <br>".$tweet->setUser(6)."<br>";
 
 
 
