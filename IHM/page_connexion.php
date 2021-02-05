bdd8a0334d904a5c148d4737e285c60d5c770958
florian60430 <florian.wantelez@hotmail.com>
2021-02-05 15:37:06 +0100

    retrait du css

--- IHM/page_connexion.php
+++ IHM/page_connexion.php
@@ -1,15 +1,8 @@
 <?php include "entete.html"; ?>
 
-
-
     <body>
         <div class="background">
             <div class="form-box">
-               <div class="button-box">
-                   <div id="btn"></div>
-                   <button type="button" class="toggle-btn" onclick="login()">Connexion</button>
-                   <button type="button" class="toggle-btn" onclick="register()">Inscription</button>
-               </div>
                <form id="login" class="input-group" method="POST">
                     <input type="text" class="input-field" placeholder="Pseudo" name="ID_1" required>
                     <input type="password" class="input-field" placeholder="Mot de passe" name="MDP_1" required>
@@ -23,25 +16,6 @@
                 </form>
             </div>
         </div>
-        <script>
-
-            var x = document.getElementById("login");
-            var y = document.getElementById("register");
-            var z = document.getElementById("btn");
-
-            function register(){
-                x.style.left = "-400px";
-                y.style.left = "50px";
-                z.style.left = "150px";
-            }
-
-            function login(){
-                x.style.left = "50px";
-                y.style.left = "450px";
-                z.style.left = "0px";
-            }
-
-        </script>
     </body>
 </html>
 
