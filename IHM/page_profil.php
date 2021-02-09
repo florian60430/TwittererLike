0d78a78f040a4bbd84c801ae2c9822547c2baa25
florian60430 <florian.wantelez@hotmail.com>
2021-02-09 08:20:19 +0100

    page profil

--- IHM/page_profil.php
+++ IHM/page_profil.php
@@ -1,55 +1,6 @@
 <?php include "structure/entete.html"; ?>
 
 
-<body>
-    <div class="background">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-3"></div>
-                <div class="col-lg-6 profil"> 
-                    salut
-                </div>
-                <div class="col-lg-3 "></div>
-            </div>
-        </div>
-
-
-
-
-
-    </div>
-</body>    
-
-
-</html>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
