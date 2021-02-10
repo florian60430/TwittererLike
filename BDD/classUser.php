26346a5f6a3d1ab55c06d6e6715afe7ee2ad082a
Clement Sene <csene@la-providence.net>
2021-02-10 13:05:57 +0100

    restrict inscription

--- BDD/classUser.php
+++ BDD/classUser.php
@@ -129,17 +129,30 @@ class user {
     ----------------------*/
 
     public function inscription($identifiant, $pseudo, $password, $birthdate){
-            
-        $verifRequest = $this->_bdd->query("INSERT INTO `user`(`id_user`, `identifiant`, `pseudo`, `password`, `birthdate`, `bio`) VALUES (NULL,'".$identifiant."','".$pseudo."','".$password."','".$birthdate."', ' ')");
-        // Manque sécurité, on peut aujouter 2x le meme user
-        if ($verifRequest)
+        
+        if(preg_match('#^[a-zA-Z0-9]*$#', $_POST['identifiant']))
         {
-            return true;
-        
-        } else {
-        
+          if(preg_match('#^[a-zA-Z0-9!:;,?()%@+-_*/=]*$#', $_POST['password'])) 
+            {     
+                $verifRequest = $this->_bdd->query("INSERT INTO `user`(`id_user`, `identifiant`, `pseudo`, `password`, `birthdate`, `bio`) VALUES (NULL,'".$identifiant."','".$pseudo."','".$password."','".$birthdate."', ' ')");
+                    // Manque sécurité, on peut aujouter 2x le meme user
+                if ($verifRequest)
+                {
+                 return true;
+            
+                } else {
+                return false;
+                }
+            } 
+          else { 
+            echo "Le mot de passe n'est pas correct"; 
+            return false;
+          }  
+        }else{
+            echo "L'identifiant n'est pas correct \n"; 
             return false;
         }
+         
     }
 
     /*------------------
--- IHM/page_connexion.php
+++ IHM/page_connexion.php
@@ -15,9 +15,9 @@
   FORMULAIRE INSCRIPTION
 -------------------------->
     <form id="register" method="POST" action="">
-        <input type="text" placeholder="Identifiant" name="identifiant" required>
+        <input type="text" placeholder="Identifiant" name="identifiant" minlength="6" maxlength="12" required>
         <input type="text" placeholder="Pseudo" name="pseudo" required>
-        <input type="password" placeholder="Mot de passe" name="password" required>
+        <input type="password" placeholder="Mot de passe" name="password" minlength="6" maxlength="15" required>
         <input type="date" placeholder="date de naissance" name="birthdate" required>
         <button type="submit">S'inscrire</button>
     </form>
