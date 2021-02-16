cf11cf0cb9ef7ec0b96cbac8f6cb014a40f381bd
florian60430 <fwantelez@la-providence.net>
2021-02-16 05:11:26 -0500

    update

---
+++ IHM/form_phone.php
@@ -0,0 +1,62 @@
+<html>
+    <head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+        <link rel="stylesheet" href="../assets/style.css">
+    </head>
+
+    <body>
+        <div class="container">
+            <div class="row">
+                <div id="logo"></div>
+            </div>   
+        <div class="row">
+            <div class="col-12" align="center">
+                <div id="titre">
+                    <h1>Twatter</h1>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12" align="center">
+                <div id="under_titre">
+                    <h2>Le meilleur reseau social de 1999</h2>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="content">
+                <p align="center" class="titre">Connexion</p>   
+                    <p class="sous_titre">Login</p>
+                        <input class="input" type="text"  name="identifiantLogin" required> 
+                    <p class="sous_titre">Mot de passe</p>
+                        <input class="input" type="password" name="identifiantLogin" required>
+                    <button class="submit" type="submit">Connexion</button>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="content">
+                <p align="center" class="titre">Inscription</p>
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <p class="sous_titre">Identifiant</p>
+                                <input class="input" type="text" name="identifiantLogin" required> 
+                            <p class="sous_titre">Login</p>
+                                <input class="input" type="text" name="identifiantLogin" required>
+                        </div>
+                        <div class="col-sm-6">
+                            <p class="sous_titre">Mot de passe</p>
+                                <input class="input" type="text" name="identifiantLogin" required>
+                            <p class="sous_titre">Date de naissance</p>
+                                <input class="input" type="text" name="identifiantLogin" required>
+                        </div>
+                    </div>
+                <button class="submit" type="submit">Inscription</button>
+            </div>
+        </div>
+
+
+        </div>
+    </body>
+</html>
