4083909012560bd091e109a38d136ab67f02d66f
root <root@florian.zuck.fun>
2021-02-16 10:47:34 -0500

    transmission de l'id stranger en GET

--- METIER/function.php
+++ METIER/function.php
@@ -20,7 +20,7 @@ function AfficheTimeLine($bdd, $ObjetUser)
             $tabOjbetTweet[$i]->like($ObjetUser);
         }
         $tabOjbetTweet[0]->CalculDate();
-        echo "<div class='tweet' name='tweet".$i."'><div class='user' name='user'> <a href='IHM/page_profil_user.php'>" . $tabOjbetUser[$i]->getPseudo() . "</a> a dit : <p><div class ='text' name='text'>" . $tabOjbetTweet[$i]->getContenu() . " </div></p><p> Date du post : " . $tabOjbetTweet[$i]->getDate() . "</p><br>";
+        echo "<div class='tweet' name='tweet".$i."'><div class='user' name='user'> <a href='IHM/page_profil_user.php?id=".$tabOjbetUser[$i]->getIdUser()."'>" . $tabOjbetUser[$i]->getPseudo() . "</a> a dit : <p><div class ='text' name='text'>" . $tabOjbetTweet[$i]->getContenu() . " </div></p><p> Date du post : " . $tabOjbetTweet[$i]->getDate() . "</p><br>";
 ?>
         </div>
             <div class='bouton' name='bouton'>
