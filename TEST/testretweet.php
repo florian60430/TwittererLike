f7910e86bf4ad75e3bb59326dc6ec2192c340bb2
marco pade <mpade@la-providence.net>
2021-02-18 12:54:29 +0100

    retweet button en cour

--- TEST/testretweet.php
+++ TEST/testretweet.php
@@ -14,9 +14,6 @@
 <head>
 
 <link rel="stylesheet" href="testcoeur.css">
-<link href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css'>
-<script src='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js'></script>
-<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
 </head>
 
 
@@ -36,7 +33,7 @@
     <div class="heartbox">
         <input  
             type="checkbox" 
-            <?php if(isset($_SESSION['islike']) && $_SESSION['islike']==true)
+            <?php if(isset($_SESSION['islike']) && ($_SESSION['islike']==true))
                     {
                         ?>
                         checked
@@ -49,17 +46,11 @@
             value="j'aime"
             onclick='setTimeout(function(){  submit(); })'
         />
-        
-        
-     
-
-      
-
       
         <label for="checkbox"> 
-            <svg id="heart-svg" viewBox="467 392 58 57" xmlns="http://www.w3.org/2000/svg">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-reply" viewBox="0 0 16 16">
                 <g id="Group" fill="none" fill-rule="evenodd" transform="translate(467 392)">
-                    <path d="M29.144 20.773c-.063-.13-4.227-8.67-11.44-2.59C7.63 28.795 28.94 43.256 29.143 43.394c.204-.138 21.513-14.6 11.44-25.213-7.214-6.08-11.377 2.46-11.44 2.59z" id="heart" fill="#AAB8C2" />
+                <path d="M6.598 5.013a.144.144 0 0 1 .202.134V6.3a.5.5 0 0 0 .5.5c.667 0 2.013.005 3.3.822.984.624 1.99 1.76 2.595 3.876-1.02-.983-2.185-1.516-3.205-1.799a8.74 8.74 0 0 0-1.921-.306 7.404 7.404 0 0 0-.798.008h-.013l-.005.001h-.001L7.3 9.9l-.05-.498a.5.5 0 0 0-.45.498v1.153c0 .108-.11.176-.202.134L2.614 8.254a.503.503 0 0 0-.042-.028.147.147 0 0 1 0-.252.499.499 0 0 0 .042-.028l3.984-2.933zM7.8 10.386c.068 0 .143.003.223.006.434.02 1.034.086 1.7.271 1.326.368 2.896 1.202 3.94 3.08a.5.5 0 0 0 .933-.305c-.464-3.71-1.886-5.662-3.46-6.66-1.245-.79-2.527-.942-3.336-.971v-.66a1.144 1.144 0 0 0-1.767-.96l-3.994 2.94a1.147 1.147 0 0 0 0 1.946l3.994 2.94a1.144 1.144 0 0 0 1.767-.96v-.667z"/>
                     <circle id="main-circ" fill="#E2264D" opacity="0" cx="29.5" cy="29.5" r="1.5" />
                     <g id="heartgroup7" opacity="0" transform="translate(7 6)">
                         <circle id="heart1" fill="#9CD8C3" cx="2" cy="6" r="2" />
